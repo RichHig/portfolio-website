e95f5991caa58db4b07b0d4a33b6cd89ada182ae
Richard Higenbottam <rich_higenbottam@hotmail.com>
2023-01-25 14:40:39 +0000

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           I'm a Frontend Developer
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm a Frontend Developer who is focused on building responsive web
+          I'm a Front-End Developer who is focused on building responsive web
           applications which meet the needs of the end users. I'm passionate,
           motivated and would be a great addition to your team.
         </p>
